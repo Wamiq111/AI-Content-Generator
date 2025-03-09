dea03e2901ba58cdffa0418179f7f8d595b4fdaf
Wamiq Ali Raja <150625301+Wamiq111@users.noreply.github.com>
2025-03-09 20:49:12 +0500

    Update Header.tsx

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,20 +1,19 @@
-import { Search } from 'lucide-react'
-import React from 'react'
+import { Search } from 'lucide-react';
+import React from 'react';
 
 function Header() {
   return (
-    <div className='-5 shadow-sm border-b-2 bg-white flex justify-between items-center'>
-      <div className='flex gap-2 items-center p-2 border rounded-md max-w-lg bg-white'>
-        <Search/>
-        <input type='text' placeholder='Search...'
-         className='outline-none' />
+    <div className='p-4 shadow-sm border-b-2 border-purple-200 bg-gradient-to-r from-purple-600 to-indigo-600 flex justify-center items-center'>
+      <div className='flex gap-2 items-center p-3 border border-purple-300 rounded-lg bg-purple-50 w-full max-w-lg shadow-sm'>
+        <Search className='text-purple-600' />
+        <input
+          type='text'
+          placeholder='Search...'
+          className='outline-none w-full text-purple-900 placeholder-purple-400 bg-transparent'
+        />
       </div>
-      <div>
-        <h2 className='bg-primary p-1 rounded-full text-xs text-white px-2'>Join Membership Only for 9.99$/Month</h2>
-      </div>
-
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
